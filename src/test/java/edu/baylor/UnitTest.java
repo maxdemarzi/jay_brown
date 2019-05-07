40cf6e3f8be44cdb2462bf5d6aa2e19b3ef4bddf
AlyssaBlack <alyssablackburn@uchicago.edu>
2019-05-07 01:24:33 -0500

    endpoint issues?

--- src/test/java/edu/baylor/UnitTest.java
+++ src/test/java/edu/baylor/UnitTest.java
@@ -25,9 +25,9 @@ public class UnitTest {
     }
 
     private static final String MODEL_STATEMENT =
-            "CREATE (c1:CARRIER { city:'city 1' })" +
-                    "CREATE (c2:CARRIER { city:'city 2' })" +
-                    "CREATE (c3:CARRIER { city:'city 3' })" +
+            "CREATE (c1:CARRIER { city:'city 1', record_num:1 })" +
+                    "CREATE (c2:CARRIER { city:'city 2', record_num:2})" +
+                    "CREATE (c3:CARRIER { city:'city 3', record_num:3})" +
 
                     "CREATE (i1:Infected { patientId:'infected 1' })" +
                     "CREATE (i2:Infected { patientId:'infected 2' })" +
@@ -83,86 +83,86 @@ public class UnitTest {
                     "CREATE (e17:INFECTION { infectionId:'event 17' })" +
                     "CREATE (e18:INFECTION { infectionId:'event 18' })" +
 
-                    "CREATE (b1)-[:OUTPUT {time:1262304000}]->(i1)" +
-                    "CREATE (b2)-[:OUTPUT {time:1262304000}]->(i2)" +
-                    "CREATE (b3)-[:OUTPUT {time:1262304000}]->(i3)" +
+                    "CREATE (b1)-[:OUTPUT {num:5}]->(i1)" +
+                    "CREATE (b2)-[:OUTPUT {num:5}]->(i2)" +
+                    "CREATE (b3)-[:OUTPUT {num:5}]->(i3)" +
 
-                    "CREATE (i1)-[:INPUT {time:1262304000}]->(e1)" +
-                    "CREATE (i2)-[:INPUT {time:1262304000}]->(e2)" +
-                    "CREATE (i3)-[:INPUT {time:1262304000}]->(e3)" +
+                    "CREATE (i1)-[:INPUT {num:5}]->(e1)" +
+                    "CREATE (i2)-[:INPUT {num:5}]->(e2)" +
+                    "CREATE (i3)-[:INPUT {num:5}]->(e3)" +
 
-                    "CREATE (e1)-[:OUTPUT {time:1262304000}]->(p1)" +
-                    "CREATE (e2)-[:OUTPUT {time:1262304000}]->(p2)" +
-                    "CREATE (e2)-[:OUTPUT {time:1262304000}]->(p3)" +
-                    "CREATE (e3)-[:OUTPUT {time:1262304000}]->(p4)" +
-                    "CREATE (e3)-[:OUTPUT {time:1262304000}]->(p5)" +
+                    "CREATE (e1)-[:OUTPUT {num:5}]->(p1)" +
+                    "CREATE (e2)-[:OUTPUT {num:5}]->(p2)" +
+                    "CREATE (e2)-[:OUTPUT {num:5}]->(p3)" +
+                    "CREATE (e3)-[:OUTPUT {num:5}]->(p4)" +
+                    "CREATE (e3)-[:OUTPUT {num:5}]->(p5)" +
 
-                    "CREATE (p1)-[:INPUT {time:1267487000}]->(e4)" +
-                    "CREATE (p2)-[:INPUT {time:1267487000}]->(e4)" +
-                    "CREATE (p3)-[:INPUT {time:1267487000}]->(e5)" +
-                    "CREATE (p4)-[:INPUT {time:1267487000}]->(e5)" +
-                    "CREATE (p5)-[:INPUT {time:1267487000}]->(e5)" +
+                    "CREATE (p1)-[:INPUT {num:10}]->(e4)" +
+                    "CREATE (p2)-[:INPUT {num:10}]->(e4)" +
+                    "CREATE (p3)-[:INPUT {num:10}]->(e5)" +
+                    "CREATE (p4)-[:INPUT {num:10}]->(e5)" +
+                    "CREATE (p5)-[:INPUT {num:10}]->(e5)" +
 
-                    "CREATE (e4)-[:OUTPUT {time:1267487000}]->(p6)" +
-                    "CREATE (e4)-[:OUTPUT {time:1267487000}]->(p7)" +
-                    "CREATE (e5)-[:OUTPUT {time:1267487000}]->(p8)" +
-                    "CREATE (e5)-[:OUTPUT {time:1267487000}]->(p9)" +
-                    "CREATE (e5)-[:OUTPUT {time:1267487000}]->(p10)" +
+                    "CREATE (e4)-[:OUTPUT {num:10}]->(p6)" +
+                    "CREATE (e4)-[:OUTPUT {num:10}]->(p7)" +
+                    "CREATE (e5)-[:OUTPUT {num:10}]->(p8)" +
+                    "CREATE (e5)-[:OUTPUT {num:10}]->(p9)" +
+                    "CREATE (e5)-[:OUTPUT {num:10}]->(p10)" +
 
 
-                    "CREATE (p11)-[:INPUT {time:1262304000}]->(e6)" +
-                    "CREATE (p12)-[:INPUT {time:1262304000}]->(e7)" +
-                    "CREATE (p13)-[:INPUT {time:1262304000}]->(e8)" +
+                    "CREATE (p11)-[:INPUT {num:5}]->(e6)" +
+                    "CREATE (p12)-[:INPUT {num:5}]->(e7)" +
+                    "CREATE (p13)-[:INPUT {num:5}]->(e8)" +
 
-                    "CREATE (e6)-[:OUTPUT {time:1262304000}]->(p11)" +
-                    "CREATE (e7)-[:OUTPUT {time:1262304000}]->(p14)" +
-                    "CREATE (e7)-[:OUTPUT {time:1262304000}]->(p15)" +
-                    "CREATE (e8)-[:OUTPUT {time:1262304000}]->(p16)" +
-                    "CREATE (e8)-[:OUTPUT {time:1262304000}]->(p17)" +
+                    "CREATE (e6)-[:OUTPUT {num:5}]->(p11)" +
+                    "CREATE (e7)-[:OUTPUT {num:5}]->(p14)" +
+                    "CREATE (e7)-[:OUTPUT {num:5}]->(p15)" +
+                    "CREATE (e8)-[:OUTPUT {num:5}]->(p16)" +
+                    "CREATE (e8)-[:OUTPUT {num:5}]->(p17)" +
 
-                    "CREATE (p17)-[:INPUT {time:1267487000}]->(e8)" +
-                    "CREATE (p18)-[:INPUT {time:1267487000}]->(e9)" +
-                    "CREATE (p19)-[:INPUT {time:1267487000}]->(e9)" +
-                    "CREATE (p20)-[:INPUT {time:1267487000}]->(e9)" +
-                    "CREATE (p15)-[:INPUT {time:1267487000}]->(e10)" +
+                    "CREATE (p17)-[:INPUT {num:10}]->(e8)" +
+                    "CREATE (p18)-[:INPUT {num:10}]->(e9)" +
+                    "CREATE (p19)-[:INPUT {num:10}]->(e9)" +
+                    "CREATE (p20)-[:INPUT {num:10}]->(e9)" +
+                    "CREATE (p15)-[:INPUT {num:10}]->(e10)" +
 
-                    "CREATE (e9)-[:OUTPUT {time:1267487000}]->(p16)" +
-                    "CREATE (e9)-[:OUTPUT {time:1267487000}]->(p17)" +
-                    "CREATE (e8)-[:OUTPUT {time:1267487000}]->(p18)" +
-                    "CREATE (e10)-[:OUTPUT {time:1267487000}]->(p19)" +
-                    "CREATE (e10)-[:OUTPUT {time:1267487000}]->(p20)"+
+                    "CREATE (e9)-[:OUTPUT {num:10}]->(p16)" +
+                    "CREATE (e9)-[:OUTPUT {num:10}]->(p17)" +
+                    "CREATE (e8)-[:OUTPUT {num:10}]->(p18)" +
+                    "CREATE (e10)-[:OUTPUT {num:10}]->(p19)" +
+                    "CREATE (e10)-[:OUTPUT {num:10}]->(p20)"+
 
-                    
+
                     //p21 interacts with p22 in T1-> p22 is not infected at all
-                    "CREATE (p21)-[:INPUT {time:1262304000}]->(e11)" +
-                    "CREATE (e11)-[:OUTPUT {time:1262304000}]->(p22)" +
+                    "CREATE (p21)-[:INPUT {num:5}]->(e11)" +
+                    "CREATE (e11)-[:OUTPUT {num:5}]->(p22)" +
 
                     //p21 is infected in T2-> p21 infected in T2
-                    "CREATE (i1)-[:INPUT {time:1267487000}]->(e12)" +
-                    "CREATE (e12)-[:OUTPUT {time:1267487000}]->(p21)" +
+                    "CREATE (i1)-[:INPUT {num:10}]->(e12)" +
+                    "CREATE (e12)-[:OUTPUT {num:10}]->(p21)" +
 
                     //p23 is infected in late T1-> p23 infected in T1
-                    "CREATE (i1)-[:INPUT {time:1262304001}]->(e13)" +
-                    "CREATE (e13)-[:OUTPUT {time:1262304001}]->(p23)" +
+                    "CREATE (i1)-[:INPUT {num:6}]->(e13)" +
+                    "CREATE (e13)-[:OUTPUT {num:6}]->(p23)" +
 
                     //p23 interacts with p24 in early T1-> should not contaminate p24 in T1
-                    "CREATE (p23)-[:INPUT {time:1262304000}]->(e14)" +
-                    "CREATE (e14)-[:OUTPUT {time:1262304000}]->(p24)" +
+                    "CREATE (p23)-[:INPUT {num:5}]->(e14)" +
+                    "CREATE (e14)-[:OUTPUT {num:5}]->(p24)" +
 
                     //p23 interacts with p24 again in T2-> now p24 is infected in T2
-                    "CREATE (p23)-[:INPUT {time:1267487000}]->(e15)" +
-                    "CREATE (e15)-[:OUTPUT {time:1267487000}]->(p24)"+
+                    "CREATE (p23)-[:INPUT {num:10}]->(e15)" +
+                    "CREATE (e15)-[:OUTPUT {num:10}]->(p24)"+
 
 
-                    //Time simultaneous infection of p25, p26, p27 in T1
-                    "CREATE (i2)-[:INPUT {time:1262304000}]->(e16)" +
-                    "CREATE (e16)-[:OUTPUT {time:1262304000}]->(p25)"+
+                    //num simultaneous infection of p25, p26, p27 in T1
+                    "CREATE (i2)-[:INPUT {num:5}]->(e16)" +
+                    "CREATE (e16)-[:OUTPUT {num:5}]->(p25)"+
 
-                    "CREATE (p25)-[:INPUT {time:1262304000}]->(e17)" +
-                    "CREATE (e17)-[:OUTPUT {time:1262304000}]->(p26)"+
+                    "CREATE (p25)-[:INPUT {num:5}]->(e17)" +
+                    "CREATE (e17)-[:OUTPUT {num:5}]->(p26)"+
 
-                    "CREATE (p26)-[:INPUT {time:1262304000}]->(e18)" +
-                    "CREATE (e18)-[:OUTPUT {time:1262304000}]->(p27)";
+                    "CREATE (p26)-[:INPUT {num:5}]->(e18)" +
+                    "CREATE (e18)-[:OUTPUT {num:5}]->(p27)";
 
 
     @Test
@@ -174,12 +174,15 @@ public class UnitTest {
             //       which my 'neo4j' rule above does.
             Session session = driver.session();
 
-            // When I use the procedure with January 1st 2010 for time, and a monthly interval with the end time 2 months later
+            // When I use the procedure with January 1st 2010 for num, and a monthly interval with the end num 2 months later
+            //StatementResult result = session.run("CALL " + Procedures.runname + "($num, $interval, $end)",
+              //      parameters("num", 5, "interval", 2592000, "end", 1264896000));
             StatementResult result = session.run("CALL " + Procedures.runname + "($time, $interval, $end)",
-                    parameters("time", 1262304000, "interval", 2592000, "end", 1264896000));
+                    parameters("time", 0, "interval", 6, "end", 6));
 
             // Then I should get what I expect
-            assertThat(result.single().get("value").asString().contains("Until period 1264896000 Num infected 9"));
+            //assertThat(result.single().get("value").asString().contains("Until period 1264896000 Num infected 9"));
+            assertThat(result.single().get("value").asString().contains("Until period 6 Num infected 12"));
         }
 
     }
@@ -193,16 +196,16 @@ public class UnitTest {
             //       which my 'neo4j' rule above does.
             Session session = driver.session();
 
-            // When I use the procedure with January 1st 2010 for time, and a monthly interval with the end time 2 months later
-            //StatementResult result = session.run("CALL " + Procedures.runname + "($time, $interval, $end)",
-            //       parameters("time", 1262304000, "interval", 2592000, "end", 1267488000));
+            // When I use the procedure with January 1st 2010 for num, and a monthly interval with the end num 2 months later
+            //StatementResult result = session.run("CALL " + Procedures.runname + "($num, $interval, $end)",
+            //       parameters("num", 5, "interval", 2592000, "end", 1267488000));
             StatementResult result = session.run("CALL " + Procedures.runname + "($time, $interval, $end)",
-                    parameters("time", 1262304000, "interval", 604800, "end", 1293840000));
+                    parameters("time", 0, "interval", 2, "end", 12));
 
             // Then I should get what I expect
-            System.out.println("Result "+result.single().get("value").asString());
-            System.out.flush();
-            assertThat(result.single().get("value").asString().contains("Until period 1267488000 Num infected 0"));
+            //System.out.println("Result "+result.single().get("value").asString());
+            //System.out.flush();
+            assertThat(result.single().get("value").asString().contains("Until period 12 Num infected 19"));
         }
     }
 
